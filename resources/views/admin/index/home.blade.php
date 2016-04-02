3001845ac7f014bbd640aaff15cbad7d5d17967b
zhaoning <zhaoning@marchsoft.cn>
2016-04-02 17:09:30 +0800

    barcode

---
+++ resources/views/admin/index/home.blade.php
@@ -0,0 +1,15 @@
+@extends('admin.layout.base')
+@section('lable')
+    <h2><i class="fa fa-home"></i> Dashboard <span>Subtitle goes here...</span></h2>
+    <div class="breadcrumb-wrapper">
+        <span class="label">You are here:</span>
+        <ol class="breadcrumb">
+            <li><a href="index.html">Bracket</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </div>
+@stop
+
+@section('contentpanel')
+    <p>This is my body content.</p>
+@stop
